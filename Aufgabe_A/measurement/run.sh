6b49d4eb152f444e008c70aa5e4714f35aea19f6
lennickbe <bengt.lennicke@mailbox.tu-dresden.de>
2023-11-27 13:24:33 +0100

    feat: first iteration of sbatch fix: position of README

---
+++ Aufgabe_A/measurement/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1                 
+#SBATCH --time=00:10:00
+#SBATCH --account=p_lv_kp_wise2324
+#SBATCH --job-name=kp_pr_aufgabe_a
+#SBATCH --output=kp_pr_aufgabe_a_%j.out
+#SBATCH --error=kp_pr_aufgabe_a_%j.err
+
+make clean
+make all
+srun './string_measurement'
