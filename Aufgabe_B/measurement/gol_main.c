039a0b32e776e6b6818ec7c6703cb0e018dbf8e1
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2023-12-06 21:43:22 +0100

    feat: scaling added to visualization feat: gifs are slower feat: file name integers include leading zeros so the gif programm takes them in order

--- Aufgabe_B/measurement/gol_main.c
+++ Aufgabe_B/measurement/gol_main.c
@@ -5,13 +5,13 @@
 void main ()
 {
     board *b;
-    char buf[12];
-    b = init_board(10,10);
-    create_pbm(b, "test.pbm");
-    for (int i = 0; i < 10; i++)
+    char buf[20];
+    b = init_board(20,20);
+    create_pbm(b, "pbms/test.pbm");
+    for (int i = 0; i < 50; i++)
     {
         update_board(b);
-        snprintf(buf, 12, "test%d.pbm", i);
+        snprintf(buf, 20, "pbms/test%03d.pbm", i);
         create_pbm(b, buf);
     }
     
--- Aufgabe_B/measurement/visualize.c
+++ Aufgabe_B/measurement/visualize.c
@@ -5,21 +5,31 @@
 void create_pbm(board *b, char *filename)
 {
     FILE *file;
-    int i, j;
-    
+    int i, j, x, y;
+
+    // scaling factor
+    int scale = 10;    
+
+
     file = fopen(filename, "wb");
     // magic number
     fprintf(file, "P1\n");
     // size
-    fprintf(file, "%d %d\n", b->cols, b->rows);
+    fprintf(file, "%d %d\n", scale*b->cols, scale*b->rows);
 
     // loop over board for picture
-    for ( i = 0; i < b-> rows; i++)
+    for ( i = 0; i < b->rows; i++)
     {
+        for ( x=0;x<scale;x++)
+        {
         for ( j= 0; j < b->cols; j++)
         {
+            for(y=0;y<scale;y++)
+            {
             fprintf(file, "%u", b->grid[ i * b->cols + j ]);
+            }
         }
         fprintf(file, "\n");
+        }
     }   
 }
