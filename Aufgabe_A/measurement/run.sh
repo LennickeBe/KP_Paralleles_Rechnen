7a9af9ee6d3eac5bd33fd372374cd99780911b76
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2023-12-24 16:13:58 +0100

    fix: sbatch script loads fixed gcc version fix: main script name corrected; runs now

--- Aufgabe_A/measurement/run.sh
+++ Aufgabe_A/measurement/run.sh
@@ -6,14 +6,9 @@
 #SBATCH --job-name=kp_pr_aufgabe_a
 #SBATCH --output=kp_pr_aufgabe_a_%j.out
 #SBATCH --error=kp_pr_aufgabe_a_%j.err
-
+ 
 module load GCCcore/10.3.0
 
-# make sure the target directory for the data is there
 mkdir -p ../evaluation/data
 
-# make sure the executable is there
-make clean
-make all
-
-srun './string_manipulation'
+srun make clean; make all; ./string_manipulation
