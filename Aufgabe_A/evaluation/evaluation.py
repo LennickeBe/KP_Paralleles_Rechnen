b248f627ea8b52df0cb101b6ff5b4b4b5f54126d
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-01-12 18:14:52 +0100

    feat: added error area around the mean for the comparison plots feat: added error bar for the complex plots

--- Aufgabe_A/evaluation/evaluation.py
+++ Aufgabe_A/evaluation/evaluation.py
@@ -107,14 +107,19 @@ def comparison_plot(strings: dict,
     plt.hlines(mean_par, series_par.index.values[0], series_par.index.values[-1], color=strings["color_par"], label="parallel mean")
     plt.hlines(mean_seq, series_seq.index.values[0], series_seq.index.values[-1], color=strings["color_seq"], label="sequential mean")
     # standard deviation / error
-    plt.hlines(mean_par+std_par, series_par.index.values[0], series_par.index.values[-1], color=strings["color_par"], alpha=0.6)
-    plt.hlines(mean_seq+std_seq, series_seq.index.values[0], series_seq.index.values[-1], color=strings["color_seq"], alpha=0.6)
-    plt.hlines(mean_par-std_par, series_par.index.values[0], series_par.index.values[-1], color=strings["color_par"], alpha=0.6)
-    plt.hlines(mean_seq-std_seq, series_seq.index.values[0], series_seq.index.values[-1], color=strings["color_seq"], alpha=0.6)
+    plt.hlines(mean_par+std_par, series_par.index.values[0], series_par.index.values[-1], color=strings["color_par"], alpha=0.4)
+    plt.hlines(mean_seq+std_seq, series_seq.index.values[0], series_seq.index.values[-1], color=strings["color_seq"], alpha=0.4)
+    plt.hlines(mean_par-std_par, series_par.index.values[0], series_par.index.values[-1], color=strings["color_par"], alpha=0.4)
+    plt.hlines(mean_seq-std_seq, series_seq.index.values[0], series_seq.index.values[-1], color=strings["color_seq"], alpha=0.4)
     # box between
-
- 
-
+    plt.fill_between([0, series_par.index.values[-1]],
+                     [mean_par+std_par, mean_par+std_par],
+                     [mean_par-std_par, mean_par-std_par],
+                     color=strings["color_par"], alpha=0.3)
+    plt.fill_between([0, series_seq.index.values[-1]],
+                     [mean_seq+std_seq, mean_seq+std_seq],
+                     [mean_seq-std_seq, mean_seq-std_seq],
+                     color=strings["color_seq"], alpha=0.3)
     # values
     plt.scatter(series_par.index.values, series_par.values, color=strings["color_par"], label="parallel", marker=strings["marker_par"])
     plt.scatter(series_seq.index.values, series_seq.values, color=strings["color_seq"], label="sequential", marker=strings["marker_seq"])
@@ -162,6 +167,8 @@ def comparison_plots(df: pd.DataFrame, str_length: int) -> None:
 def complex_plot(strings: dict,
                  means_par: list,
                  means_seq: list,
+                 std_par: list,
+                 std_seq: list,
                  iter_list: list) -> None:
     fig, ax = plt.subplots()
     plt.title(strings["title"], pad=20)
@@ -169,8 +176,8 @@ def complex_plot(strings: dict,
     plt.ylabel(strings["ylabel"])
     ax.set_xscale("log")
     ax.set_yscale("log")
-    plt.scatter(iter_list, means_par, color=strings["color_par"], marker=strings["marker_par"], label="mean parallel")
-    plt.scatter(iter_list, means_seq, color=strings["color_par"], marker=strings["marker_seq"], label="mean sequential")
+    plt.errorbar(iter_list, means_par, yerr=std_par, color=strings["color_par"],fmt=strings["marker_par"], label="mean parallel")
+    plt.errorbar(iter_list, means_seq, yerr=std_seq, color=strings["color_seq"], fmt=strings["marker_seq"], label="mean sequential")
     plt.legend()
     plt.savefig(strings["filename"], bbox_inches = "tight")
     #plt.show()
@@ -189,11 +196,15 @@ def complex_plots(df_10k: pd.DataFrame,
     # count
     means_par = []
     means_seq = []
+    std_par = []
+    std_seq = []
     for df in df_list:
         means_par.append(np.mean(df["count_par"]))
         means_seq.append(np.mean(df["count_seq"]))
+        std_par.append(np.std(df["count_par"]))
+        std_seq.append(np.std(df["count_seq"]))
     strings = get_strings("complex_count")
-    complex_plot(strings, means_par, means_seq, iter_list)
+    complex_plot(strings, means_par, means_seq, std_par, std_seq, iter_list)
     
     # upper
     means_par = []
@@ -202,7 +213,7 @@ def complex_plots(df_10k: pd.DataFrame,
         means_par.append(np.mean(df["upper_par"]))
         means_seq.append(np.mean(df["upper_seq"]))
     strings = get_strings("complex_up")
-    complex_plot(strings, means_par, means_seq, iter_list)
+    complex_plot(strings, means_par, means_seq, std_par, std_seq, iter_list)
 
     # lower
     means_par = []
@@ -211,7 +222,7 @@ def complex_plots(df_10k: pd.DataFrame,
         means_par.append(np.mean(df["lower_par"]))
         means_seq.append(np.mean(df["lower_seq"]))
     strings = get_strings("complex_low")
-    complex_plot(strings, means_par, means_seq, iter_list)
+    complex_plot(strings, means_par, means_seq, std_par, std_seq, iter_list)
 
 def write_tab_line(f,
                    iterations :int,
