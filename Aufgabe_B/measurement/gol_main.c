efbe3c607ef808dc094fe2f8e697a85d27d32896
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-01-14 14:28:21 +0100

    fix: identation now the one from laptop

--- Aufgabe_B/measurement/gol_main.c
+++ Aufgabe_B/measurement/gol_main.c
@@ -12,45 +12,39 @@ float get_time_diff_in_s(struct timespec start, struct timespec end)
 }
 
 
+void measure(size_t board_axis, int iterations)
+{
+
+	board *b, *b1;
+	char buf[20];
+	struct timespec start, end;
+
+	b = init_board(200,200, 5000);//(board_axis*board_axis)/8 );
+	b1 = create_board_copy(b);
+
+	// parallel
+	create_pbm(b1, "pbms/foo.pbm");
+	for (int j = 0; j < 1; j++)
+	{
+		b1 = create_board_copy(b);
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for (int i = 0; i < iterations; i++)
+		{
+			update_board_threaded(b1);
+			snprintf(buf, 20, "pbms/foo%03d.pbm", i);
+			create_pbm(b1, buf);
+		}
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		printf("start: %lds %ldns\nend: %lds %ldns\n", start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
+		printf("\t = %2.4fs\n", get_time_diff_in_s(start, end));
+		}
+	}
+
+
+
+
 
 void main ()
 {
-    board *b, *b1, *b2;
-    char buf[20];
-    struct timespec start, end;
-
-
-    b = init_board(1000,1000, 150000);
-    b1 = create_board_copy(b);
-    b2 = create_board_copy(b);
-   
-    // sequential
-    //create_pbm(b1, "pbms/test.pbm");
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (int i = 0; i < 80; i++)
-    {
-        update_board(b1);
-        //snprintf(buf, 20, "pbms/test%03d.pbm", i);
-        //create_pbm(b1, buf);
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("start: %lds %ldns\nend: %lds %ldns\n", start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
-    printf("\t = %2.4fs\n", get_time_diff_in_s(start, end));
-
-    // parallel
-    //create_pbm(b1, "pbms/foo.pbm");
-    for (int j = 0; j < 20; j++)
-    {
-    b2 = create_board_copy(b);
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (int i = 0; i < 80; i++)
-    {
-        update_board_threaded(b2);
-        //snprintf(buf, 20, "pbms/foo%03d.pbm", i);
-        //create_pbm(b2, buf);
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("start: %lds %ldns\nend: %lds %ldns\n", start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
-    printf("\t = %2.4fs\n", get_time_diff_in_s(start, end));
-    }
+	measure(128, 10);
 }
