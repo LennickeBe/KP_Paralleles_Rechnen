8a80c1f2566ed53a48be013de85bea268eaf9516
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-01-15 12:18:42 +0100

    feat: measurement now saves measured times in struct and writes the results after measuring

--- Aufgabe_B/measurement/gol_main.c
+++ Aufgabe_B/measurement/gol_main.c
@@ -1,15 +1,41 @@
 #include "gol_board.h"
 #include "visualize.h"
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
+struct times {
+	struct timespec *starts;
+	struct timespec *ends;
+};
 
-float get_time_diff_in_s(struct timespec start, struct timespec end)
+
+
+float get_time_diff_in_s(struct timespec *start, struct timespec *end)
 {
-	return ((end.tv_sec - start.tv_sec) +
-		((end.tv_nsec - start.tv_nsec) / 1000000000.0));
+	return ((end->tv_sec - start->tv_sec) +
+		((end->tv_nsec - start->tv_nsec) / 1000000000.0));
 }
+
+void write_times(FILE *file, int num, struct times *meas_times)
+{
+	int i;
+	struct timespec *start, *end;
+ 
+	// use local scope pointer to move in mem(and improve readability)
+	start = meas_times->starts;
+	end = meas_times->ends;
+
+	for (i = 0; i<num; i++)
+	{
+		fprintf(file, "start: %lds %ldns\nend: %lds %ldns\n", start->tv_sec, start->tv_nsec, end->tv_sec, end->tv_nsec);
+		fprintf(file, "\t = %2.4fs\n", get_time_diff_in_s(start, end));
+		start += sizeof(struct timespec);
+		end += sizeof(struct timespec);
+	}
+}
+
 
 
 void visualize(size_t board_axis, int iterations)
@@ -28,47 +54,65 @@ void visualize(size_t board_axis, int iterations)
 }
 
 
-void measure(size_t board_axis, int measurement_num, int iterations)
+void measure(size_t board_axis,
+		int measurement_num,
+		int iterations,
+		struct times *meas_times)
 {
 
 	board *b, *b1;
-	struct timespec start, end;
+	struct timespec *start, *end;
 
+	// allocate mem for the measurement times
+	meas_times->starts = malloc(measurement_num * sizeof(struct timespec));
+	meas_times->ends = malloc(measurement_num * sizeof(struct timespec));
+	// use local scope pointer to move in mem(and improve readability)
+	start = meas_times->starts;
+	end = meas_times->ends;
+
+	// start board with 20%~ of the cells alive
 	b = init_board(board_axis, board_axis,(board_axis*board_axis)/8 );
-	b1 = create_board_copy(b);
 
-	// parallel
+	// play the game with the same board multiple times
 	for (int j = 0; j < measurement_num; j++)
 	{
+		// reset board
 		b1 = create_board_copy(b);
-		clock_gettime(CLOCK_MONOTONIC, &start);
+		clock_gettime(CLOCK_MONOTONIC, start);
+		// loop over game states
 		for (int i = 0; i < iterations; i++)
 		{
 			update_board_threaded(b1);
 		}
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		//printf("start: %lds %ldns\nend: %lds %ldns\n", start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
-		//printf("\t = %2.4fs\n", get_time_diff_in_s(start, end));
+		clock_gettime(CLOCK_MONOTONIC, end);
+		// move pointer in timespec areas
+		start += sizeof(struct timespec);
+		end += sizeof(struct timespec);
 	}
-	printf("start: %lds %ldns\nend: %lds %ldns\n", start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
-	printf("\t = %2.4fs\n", get_time_diff_in_s(start, end));
-
 }
 
 
 void main ()
 {
+	struct times times_128;
+	FILE *file;
+
 	//visualize(200, 10);
 	
 	//128x128
-	measure(128, 1, 1);
+	measure(128, 10, 10, &times_128);
 	//512x512
-	measure(512, 1, 1);
+	//measure(512, 1, 1);
 	//2048x2048
-	measure(2048, 1, 1);
+	//measure(2048, 1, 1);
 	//8192x8192
-	measure(8192, 1, 1);
+	//measure(8192, 1, 1);
 	//32768x32768
-	measure(32768, 1, 1);
+	//measure(32768, 1, 1);	
+	
+	// write results
+	file = fopen("times.csv", "w");
+	write_times(file,10,  &times_128);
+
 	
 }
