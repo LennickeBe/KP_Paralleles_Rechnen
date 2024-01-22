2ec699aa2b23b0c21178802fc4102936f530d521
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-01-22 16:09:40 +0100

    feat: evaluation plots comparison of threads for fixed size

--- Aufgabe_B/evaluation/evaluation.py
+++ Aufgabe_B/evaluation/evaluation.py
@@ -6,6 +6,30 @@ import matplotlib.pyplot as plt
 thread_paths = ["1", "2", "4", "8", "16", "32"]
 board_sizes = ["128", "512", "2048", "8192", "32768"]
 
+def fix_size_plot(size: int, gcc_data: dict, icc_data):
+    fig = plt.subplots()
+    plt.title("Durchschnittliche Laufzeit von update_board() mit "
+            + size + "x" + size + " Boardgröße.")
+    plt.ylabel("Laufzeit [s]")
+    plt.xlabel("Threadanzahl")
+    plt.errorbar(thread_paths,
+            [ y[size][0] for x,y in gcc_data.items()],
+            yerr=[ y[size][1] for x,y in gcc_data.items()],
+            color="green", fmt="^", label="gcc", capsize=4)
+    plt.errorbar(thread_paths,
+            [ y[size][0] for x,y in icc_data.items()],
+            yerr=[ y[size][1] for x,y in icc_data.items()],
+            color="orange", fmt="s", label="icc", capsize=4)
+    plt.legend()
+    plt.show()
+
+
+def compiler_compare_plots(gcc_data: dict, icc_data: dict):
+    # fixed size multiple threads
+    for size in board_sizes:
+        fix_size_plot(size, gcc_data, icc_data)
+    # fixed threads mulitple sized
+
 
 def df_to_mean_std(data: dict):
     result = dict()
@@ -13,7 +37,7 @@ def df_to_mean_std(data: dict):
         result[thread] = dict()
         for size in board_sizes:
             result[thread][size] = (np.mean(data[thread][size]),
-                np.std(data[thread][size]))
+                np.std(data[thread][size].values))
     return result
 
 
@@ -44,14 +68,17 @@ def main():
                                 thread,
                                 f"times{size}.csv")
             thread_data[size] = pd.read_csv(path)
-
         icc_data[thread] = thread_data
 
     gcc_data = df_to_mean_std(gcc_data)
+    icc_data = df_to_mean_std(icc_data)
+    
+    compiler_compare_plots(gcc_data, icc_data)
+    return
     for i in gcc_data.items():
-        print(f"{i[0]}\n")
+        print(f"\n{i[0]}:")
         for j in i[1].items():
-            print(j[1].head(5))
+            print(j)
 
 
 if __name__=="__main__":
