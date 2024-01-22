8ae6261f118219d3469a74ad6be373dd5849f503
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-01-22 17:21:38 +0100

    feat: fix number of threads plots added fix: thread/boardsize array now integers so spaced correct on x axis

--- Aufgabe_B/evaluation/evaluation.py
+++ Aufgabe_B/evaluation/evaluation.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-thread_paths = ["1", "2", "4", "8", "16", "32"]
-board_sizes = ["128", "512", "2048", "8192", "32768"]
+thread_paths = [1, 2, 4, 8, 16, 32]
+board_sizes = [128, 512, 2048, 8192, 32768]
 
-def fix_size_plot(size: int, gcc_data: dict, icc_data):
+def fix_size_plot(size: int, gcc_data: dict, icc_data: dict):
     fig = plt.subplots()
     plt.title("Durchschnittliche Laufzeit von update_board() mit "
-            + size + "x" + size + " Boardgröße.")
+            + str(size) + "x" + str(size) + " Boardgröße.")
     plt.ylabel("Laufzeit [s]")
     plt.xlabel("Threadanzahl")
     plt.errorbar(thread_paths,
@@ -24,11 +24,37 @@ def fix_size_plot(size: int, gcc_data: dict, icc_data):
     plt.show()
 
 
+def fix_threads_plot(threads: int, gcc_data: dict, icc_data: dict):
+    fig,ax = plt.subplots()
+    if threads==1:
+        plt.title("Durchschnittliche Laufzeit von update_board() mit 1 Thread.")
+    else:
+        plt.title("Durchschnittliche Laufzeit von update_board() mit "
+            + str(threads) + " Threads.")
+    plt.ylabel("Laufzeit [s]")
+    plt.xlabel("Boardsize")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    plt.errorbar(board_sizes,
+            [ gcc_data[size][0] for size in board_sizes],
+            yerr=[ gcc_data[size][1] for size in board_sizes],
+            color="green", fmt="^", label="gcc", capsize=4)
+    plt.errorbar(board_sizes,
+            [ icc_data[size][0] for size in board_sizes],
+            yerr=[ icc_data[size][1] for size in board_sizes],
+            color="orange", fmt="s", label="icc", capsize=4)
+    plt.legend()
+    plt.show()
+
+
 def compiler_compare_plots(gcc_data: dict, icc_data: dict):
     # fixed size multiple threads
     for size in board_sizes:
+        break
         fix_size_plot(size, gcc_data, icc_data)
     # fixed threads mulitple sized
+    for threads in thread_paths:
+        fix_threads_plot(threads, gcc_data[threads], icc_data[threads])
 
 
 def df_to_mean_std(data: dict):
@@ -53,7 +79,7 @@ def main():
         for size in board_sizes:
             path = os.path.join(data_path,
                                 gcc_path,
-                                thread,
+                                str(thread),
                                 f"times{size}.csv")
             thread_data[size] = pd.read_csv(path)
 
@@ -65,7 +91,7 @@ def main():
         for size in board_sizes:
             path = os.path.join(data_path,
                                 icc_path,
-                                thread,
+                                str(thread),
                                 f"times{size}.csv")
             thread_data[size] = pd.read_csv(path)
         icc_data[thread] = thread_data
