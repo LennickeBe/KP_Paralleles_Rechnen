b7c54e46c6c7f2c6f52f17db7ff8462a9414b665
lennickebe <bengt.lennicke@mailbox.tu-dresden.de>
2024-04-22 15:18:30 +0200

    all: fix: changed sbatch account so it runs on slurm again

--- Aufgabe_A/measurement/run.sh
+++ Aufgabe_A/measurement/run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --ntasks=1                 
 #SBATCH --time=00:10:00
-#SBATCH --account=p_lv_kp_wise2324
+#SBATCH --account=p_tei_ra_hpc_courses
 #SBATCH --job-name=kp_pr_aufgabe_a
 #SBATCH --output=kp_pr_aufgabe_a_%j.out
 #SBATCH --error=kp_pr_aufgabe_a_%j.err
--- Aufgabe_B/measurement/slurm.sh
+++ Aufgabe_B/measurement/slurm.sh
@@ -2,10 +2,12 @@
 
 #SBATCH --ntasks=16
 #SBATCH --time=04:30:00
-#SBATCH --account=p_lv_kp_wise2324
+#SBATCH --account=p_tei_ra_hpc_courses
 #SBATCH --job-name=kp_pr_aufgabe_b
 #SBATCH --output=kp_pr_aufgabe_a_%j.out
 #SBATCH --error=kp_pr_aufgabe_a_%j.err
+#SBATCH --mem=5G
+#SBATCH --cpus-per-task=32
 
 # make sure run.sh is executable
 chmod u+x run.sh
